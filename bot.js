69f4de81f4f47e110516722c4692e5666d8a80f9
Roni Fernando <ronifernando.id@gmail.com>
2018-04-16 19:43:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,24 @@
-const botconfig = require('./botconfig.json');
+const botconfig = require("./botconfig.json");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('I am ready!');
  
     client.user.setPresence({ game: { name: '=help', type: 2 } });
 });
 
-client.on('message', message => {    
+client.on('message', async message => {  
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     
     
-    if (cmd === '${prefix}ping') {
+    if (cmd === ${prefix}'ping') {
     	message.channel.send('PONG!');
   	}
     
