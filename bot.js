e76d8a9ae900ed608f1ac75c8b554c60099f905c
ronifernando <ronifernando.id@gmail.com>
2018-04-17 05:05:47 +0700

    update

--- bot.js
+++ bot.js
@@ -1,17 +1,19 @@
 const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
-const YoutubeDL = require('youtube-dl');
-const ytdl = require('ytdl-core');
+const Music = require('discord.js-musicbot-addon');
 
 const client = new Discord.Client();
 
 let PREFIX = botconfig.prefix;
-let servers = {};
 
 client.on('ready', async () => {
     console.log('I am ready!');
 
     client.user.setPresence({ game: { name: '-help', type: 2 } });
+});
+
+const music = new Music(client, {
+  youtubeKey: 'AIzaSyAMpPZdsqJxBySqctF0YDiFYaHnZClCuwg'
 });
 
 client.on('message', async message => {
@@ -34,56 +36,10 @@ client.on('message', async message => {
                 message.channel.send("anda bukan admin!");
             }
             break;
-        case 'play':
-            play(message, args[1]);
-            break;
-        case 'skip':
-            var server = servers[message.guild.id];
-            if(server.dispatcher) server.dispatcher.end();
-            break;
-        case 'stop':
-            var server = servers[message.guild.id];
-            if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-            break;
         default:
             message.channel.sendMessage("Command tidak ada");
     }
 });
-
-function play(msg, title){
-  if(!title){
-    msg.channel.sendMessage("Link belum ada");
-    return;
-  }
-  if(!msg.member.voiceChannel){
-    msg.channel.sendMessage("Harus masuk voice channel");
-    return;
-  }
-  if(!servers[msg.guild.id]) servers[msg.guild.id] = {
-    queue: []
-  };
-
-  var server = servers[msg.guild.id];
-
-  server.queue.push(title);
-
-  if (!msg.guild.voiceConnection) msg.member.voiceChannel.join().then(function(connection){
-    playcon(connection, msg);
-  });
-}
-
-function playcon(connection, message){
-  var server = servers[message.guild.id];
-
-  server.dispatcher = connection.playStream(ytdl('https://www.youtube.com/watch?v=d9IxdwEFk1c', {filter: "audioonly"}));
-
-  server.queue.shift();
-
-  server.dispatcher.on("end", function(){
-    if (server.queue[0]) playcon(connection, message);
-    else connection.disconnect();
-  });
-}
 
 function isAdmin(member) {
   return member.hasPermission("ADMINISTRATOR");
