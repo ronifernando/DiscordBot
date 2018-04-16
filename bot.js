962d702cb7bbc52e84cc13cb1cde6957df8505ec
Roni Fernando <ronifernando.id@gmail.com>
2018-04-16 18:56:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     
-    client.user.setGame('cek');
+    client.User.setGame('cek');
 });
 
 client.on('message', message => {
