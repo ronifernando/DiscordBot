3a6e89ccd710969fe1ca8db4797c3a4a70710d1b
Roni Fernando <ronifernando.id@gmail.com>
2018-04-17 02:24:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
-const yt = require('ytdl-core');
 const music = require('discord.js-music-v11');
 
 const client = new Discord.Client();
@@ -9,25 +8,25 @@ let queues = {};
 
 client.on('ready', async () => {
     console.log('I am ready!');
- 
+
     client.user.setPresence({ game: { name: '-help', type: 2 } });
 });
 
 music(client, {
-	prefix: botconfig.prefix,   
-	global: false,     
-	maxQueueSize: 10, 
+	prefix: botconfig.prefix,
+	global: false,
+	maxQueueSize: 10,
 });
 
-client.on('message', async msg => {  
+client.on('message', async msg => {
     if(msg.author.bot) return;
     if(msg.channel.type === "dm") return;
     if(!msg.content.startsWith(botconfig.prefix)) return;
-   
+
     var args = msg.content.substring(botconfig.prefix.length).split(" ");
     var cmd = args[0];
     var args1 = args.slice(1);
-	
+
     switch (cmd.toLowerCase()){
         case "help":
             msg.channel.send("Under Development!");
@@ -41,14 +40,7 @@ client.on('message', async msg => {
             break;
         default:
             message.channel.sendMessage("Command tidak ada");
-    }    
+    }
 });
-function getQueue(server) {
-		if (GLOBAL) server = '_'; // Change to global queue.
-
-		// Return the queue.
-		if (!queues[server]) queues[server] = [];
-		return queues[server];
-}
 
 client.login(process.env.BOT_TOKEN);
