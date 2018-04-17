26f1bb376e6c7e745cd123020edcf81bf1187267
ronifernando <ronifernando.id@gmail.com>
2018-04-17 07:45:07 +0700

    update

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', async () => {
     client.user.setPresence({ game: { name: '-help', type: 2 } });
 });
 
-Client.on('message' async message => {
+client.on('message' async message => {
   if (isDJ(message.member)){
     dj = true;
   }else{
